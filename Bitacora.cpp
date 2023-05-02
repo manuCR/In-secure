de1562c77b64c8e95758a8afa9c52ac6d330c11b
VALERY.MURCIA@ucr.ac.cr <VALERY.MURCIA@ucr.ac.cr>
2023-05-02 16:38:16 -0600

    Added methods of open write and close

--- Bitacora.cpp
+++ Bitacora.cpp
@@ -8,6 +8,7 @@ using namespace std;
 // Constructor
 Bitacora::Bitacora() {
     filename="bitacora.txt";
+    ofstream archivo;
 }
 
 void Bitacora::add(string entry) {
@@ -21,3 +22,22 @@ void Bitacora::add(string entry) {
     archivo.close();
     cout << "Escrito correctamente";
 }
+
+
+void Bitacora::open(){
+    archivo.open(filename.c_str(), fstream::out);
+    cout<< "Se abrió el archivo" <<endl;
+    auto now = std::chrono::system_clock::now();
+    std::time_t end_time = std::chrono::system_clock::to_time_t(now);
+    archivo << std::ctime(&end_time)<< "\n";
+    //write(entry);
+  }
+
+void Bitacora::write(string entry){
+    archivo << entry<<"\n";
+  }
+
+void Bitacora::close(){
+    archivo.close();
+    cout<< "Se cerró el archivo" <<endl;
+  }
--- Bitacora.hpp
+++ Bitacora.hpp
@@ -9,6 +9,7 @@ class Bitacora {
 
  public:
   std::string filename;
+  ofstream archivo;
 
   /**
   * @brief Class constructor.
@@ -17,6 +18,13 @@ class Bitacora {
 
   void add(std::string entry);
 
+  private: 
+
+  void open();
+
+  void write(std::string entry);
+
+  void close();
 
 };
 #endif  // BITACORA_H
