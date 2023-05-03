95376ea7c782c10362589f5beb889b315e16f82b
VALERY.MURCIA@ucr.ac.cr <VALERY.MURCIA@ucr.ac.cr>
2023-05-03 15:28:48 -0600

    Added Escritor class to use for the final machine of the ring, and changed the bitacora class to use openlog, syslog,closelog

--- Bitacora.cpp
+++ Bitacora.cpp
@@ -1,43 +1,37 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-using namespace std;
+#include <syslog.h>
 #include "Bitacora.hpp"
-#include <chrono>
 
 // Constructor
 Bitacora::Bitacora() {
-    filename="bitacora.txt";
-    ofstream archivo;
+
 }
 
 void Bitacora::add(string entry) {
-    ofstream archivo;
-    archivo.open(filename.c_str(), fstream::out);
-    auto now = std::chrono::system_clock::now();
-    std::time_t end_time = std::chrono::system_clock::to_time_t(now);
-    archivo << std::ctime(&end_time);
-    archivo << entry;
-    archivo << endl;
-    archivo.close();
-    cout << "Escrito correctamente";
+    // Se abre la conexión con el servicio syslog
+    openlog("Main", LOG_CONS | LOG_PID, LOG_LOCAL5);
+
+    // Se escribe un mensaje de prueba en el nivel de severidad 'info'
+    syslog(LOG_INFO, entry);
+
+    // Se cierra la conexión con el servicio syslog
+    closelog();    
 }
 
 
 void Bitacora::open(){
-    archivo.open(filename.c_str(), fstream::out);
-    cout<< "Se abrió el archivo" <<endl;
-    auto now = std::chrono::system_clock::now();
-    std::time_t end_time = std::chrono::system_clock::to_time_t(now);
-    archivo << std::ctime(&end_time)<< "\n";
-    //write(entry);
-  }
+    // Se abre la conexión con el servicio syslog
+    openlog("Main", LOG_CONS | LOG_PID, LOG_LOCAL5);
+      }
 
 void Bitacora::write(string entry){
-    archivo << entry<<"\n";
+    // Se escribe un mensaje de prueba en el nivel de severidad 'info'
+    syslog(LOG_INFO, entry);
   }
 
 void Bitacora::close(){
-    archivo.close();
-    cout<< "Se cerró el archivo" <<endl;
+    // Se cierra la conexión con el servicio syslog
+    closelog();
   }
+
+
+
