90c29b905eb2b6567e638b1e3b09f29e7dd5d92b
manuCR <manuelarroyoportilla@gmail.com>
2023-05-04 05:21:03 -0600

    arreglo en socket ya funciona en la NAC

--- Socket.cpp
+++ Socket.cpp
@@ -1,6 +1,9 @@
 #include "Socket.h"
 #include <arpa/inet.h>
+#include <cerrno>
+#include <cstring>
 #include <iostream>
+#include <cstdlib>
 #include <sys/socket.h>
 #include <unistd.h>
 
@@ -20,7 +23,7 @@ void Socket::connectTo(const std::string &address, int port) {
 
   if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) ==
       -1) {
-    std::cerr << "Failed to connect to server" << std::endl;
+    std::cerr << "Failed to connect to server: " << std::strerror(errno) << std::endl;
     exit(EXIT_FAILURE);
   }
 }
@@ -35,27 +38,27 @@ void Socket::bindTo(const std::string &address, int port) {
   server_addr.sin_family = AF_INET;
   server_addr.sin_port = htons(port);
   server_addr.sin_addr.s_addr = inet_addr(address.c_str());
-  int addrlen = sizeof(address);
+  int addrlen = sizeof(server_addr);
 
   if (bind(sockfd, (struct sockaddr *)&server_addr, addrlen) == -1) {
-    std::cerr << "Failed to connect to server" << std::endl;
+    std::cerr << "Failed to connect to server: " << std::strerror(errno) << std::endl;
     exit(EXIT_FAILURE);
   }
 
   if (listen(sockfd, 3) == -1) {
-    std::cerr << "Failed to listen to server" << std::endl;
+    std::cerr << "Failed to listen to server: " << std::strerror(errno) << std::endl;
     exit(EXIT_FAILURE);
   }
   new_socket =
       accept(sockfd, (struct sockaddr *)&server_addr, (socklen_t *)&addrlen);
   if (new_socket == -1) {
-    std::cerr << "Failed to accept to server" << std::endl;
+    std::cerr << "Failed to accept to server: " << std::strerror(errno) << std::endl;
   }
 }
 
 void Socket::send(const std::string &message) {
   if (::send(sockfd, message.c_str(), message.size(), 0) == -1) {
-    std::cerr << "Failed to send message" << std::endl;
+    std::cerr << "Failed to send message: " << std::strerror(errno) << std::endl;
     exit(EXIT_FAILURE);
   }
 }
@@ -63,7 +66,7 @@ void Socket::send(const std::string &message) {
 std::string Socket::receive(int buffer_size) {
   char buffer[buffer_size];
   if (read(new_socket, buffer, buffer_size) == -1) {
-    std::cerr << "Failed to receive message" << std::endl;
+    std::cerr << "Failed to receive message: " << std::strerror(errno) << std::endl;
     exit(EXIT_FAILURE);
   }
   return std::string(buffer);
