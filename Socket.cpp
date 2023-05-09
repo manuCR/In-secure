6d2ce48a3be898819ac28914a874e35587746f70
manuCR <manuelarroyoportilla@gmail.com>
2023-05-08 21:58:29 -0600

    revert

--- Socket.cpp
+++ Socket.cpp
@@ -21,7 +21,7 @@ Socket::Socket(std::string address, int port) {
 }
 
 void Socket::connectTo() {
-  if (connect(sockfd, (struct sockaddr *)&server_addr, (socklen_t *)&addrlen) ==
+  if (connect(sockfd, (struct sockaddr *)&server_addr, addrlen) ==
       -1) {
     std::cerr << "Failed to connect to server: " << std::strerror(errno)
               << std::endl;
@@ -31,7 +31,7 @@ void Socket::connectTo() {
 
 void Socket::bindTo() {
   int opt = 1;
-  if (bind(sockfd, (struct sockaddr *)&server_addr, (socklen_t *)&addrlen) == -1) {
+  if (bind(sockfd, (struct sockaddr *)&server_addr, addrlen) == -1) {
     std::cerr << "Failed to connect to server: " << std::strerror(errno)
               << std::endl;
     exit(EXIT_FAILURE);
