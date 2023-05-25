567a67b0fddee0befb8fb5cd4d17965edae6c074
Maevas <valery.murcia@ucr.ac.cr>
2023-05-24 22:01:02 -0600

    Added cifrado class

--- Cifrado.cpp
+++ Cifrado.cpp
@@ -1,24 +1,115 @@
 #include <iostream>
 #include <cstdlib>
+#include <string>
+#include <fstream>
+
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+#include <openssl/err.h>
+
+std::string encryptMessage(const std::string& message, const std::string& publicKeyPath) {
+    // Cargar la clave pública
+    FILE* publicKeyFile = fopen(publicKeyPath.c_str(), "rb");
+    if (!publicKeyFile) {
+        std::cerr << "Error al abrir el archivo de clave pública" << std::endl;
+        return "";
+    }
+
+    RSA* rsa = PEM_read_RSA_PUBKEY(publicKeyFile, NULL, NULL, NULL);
+    fclose(publicKeyFile);
+
+    if (!rsa) {
+        std::cerr << "Error al leer la clave pública" << std::endl;
+        return "";
+    }
+
+    // Obtener el tamaño máximo del bloque de encriptación
+    int maxLength = RSA_size(rsa) - 42; // RSA_PKCS1_PADDING
+
+    // Preparar el buffer de salida
+    std::string encryptedMessage;
+    encryptedMessage.resize(RSA_size(rsa));
+
+    // Encriptar el mensaje
+    int encryptedLength = RSA_public_encrypt(message.size(), reinterpret_cast<const unsigned char*>(message.data()),
+                                             reinterpret_cast<unsigned char*>(&encryptedMessage[0]), rsa, RSA_PKCS1_PADDING);
+
+    RSA_free(rsa);
+
+    if (encryptedLength == -1) {
+        std::cerr << "Error al encriptar el mensaje" << std::endl;
+        return "";
+    }
+
+    // Ajustar el tamaño del mensaje encriptado
+    encryptedMessage.resize(encryptedLength);
+
+    return encryptedMessage;
+}
+
+std::string decryptMessage(const std::string& encryptedMessage, const std::string& privateKeyPath) {
+    // Cargar la clave privada
+    FILE* privateKeyFile = fopen(privateKeyPath.c_str(), "rb");
+    if (!privateKeyFile) {
+        std::cerr << "Error al abrir el archivo de clave privada" << std::endl;
+        return "";
+    }
+
+    RSA* rsa = PEM_read_RSAPrivateKey(privateKeyFile, NULL, NULL, NULL);
+    fclose(privateKeyFile);
+
+    if (!rsa) {
+        std::cerr << "Error al leer la clave privada" << std::endl;
+        return "";
+    }
+
+    // Preparar el buffer de salida
+    std::string decryptedMessage;
+    decryptedMessage.resize(RSA_size(rsa));
+
+    // Descifrar el mensaje
+    int decryptedLength = RSA_private_decrypt(encryptedMessage.size(),
+                                               reinterpret_cast<const unsigned char*>(encryptedMessage.data()),
+                                               reinterpret_cast<unsigned char*>(&decryptedMessage[0]), rsa,
+                                               RSA_PKCS1_PADDING);
+
+    RSA_free(rsa);
+
+    if (decryptedLength == -1) {
+        std::cerr << "Error al descifrar el mensaje" << std::endl;
+        return "";
+    }
+
+    // Ajustar el tamaño del mensaje descifrado
+    decryptedMessage.resize(decryptedLength);
+
+    return decryptedMessage;
+}
 
 int main() {
-    // Generar clave RSA
-    //std::system("openssl genrsa -out key.pem 2048");
+    std::string message = "un patito muy feito, era muy queridito";
+    std::string publicKeyPath = "/home/valery.murcia/In-secure/pub.pem";
+    std::string privateKeyPath = "/home/valery.murcia/In-secure/key.pem";
 
-    // Mostrar información de la clave privada
-    //std::system("openssl rsa -in key.pem -text -noout");
+    // Encriptar el mensaje
+    std::string encryptedMessage = encryptMessage(message, publicKeyPath);
 
-    // Guardar clave pública en el archivo "pub.pem"
-    //std::system("openssl rsa -in key.pem -pubout -out pub.pem");
+    if (encryptedMessage.empty()) {
+        std::cerr << "Error al encriptar el mensaje" << std::endl;
+        return 1;
+    }
 
-    // Mostrar información de la clave pública
-    //std::system("openssl rsa -in pub.pem -pubin -text -noout");
+    std::cout << "Mensaje encriptado: " << encryptedMessage << std::endl;
 
-    // Encriptar datos
-    std::system("openssl rsautl -encrypt -inkey pub.pem -pubin -in Archivo.txt -out Archivo.bin");
+    // Descifrar el mensaje
+    std::string decryptedMessage = decryptMessage(encryptedMessage, privateKeyPath);
 
-    // Desencriptar datos encriptados
-    std::system("openssl rsautl -decrypt -inkey key.pem -in Archivo.bin");
+    if (decryptedMessage.empty()) {
+        std::cerr << "Error al descifrar el mensaje" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Mensaje descifrado: " << decryptedMessage << std::endl;
 
     return 0;
 }
