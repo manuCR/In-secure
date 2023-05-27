34ac38509cd76ae1a8a258cb3c482f465b2fc924
manuCR <manuelarroyoportilla@gmail.com>
2023-05-27 17:56:12 -0600

    fixes

--- Cifrado.cpp
+++ Cifrado.cpp
@@ -29,9 +29,9 @@ std::string Cifrado::encryptMessage(const std::string& message, const std::strin
     }
 
     std::cout << "Tamaño máximo  antes " << std::endl;
-    int maxLength = RSA_public_encrypt(0, nullptr, nullptr, rsa, RSA_PKCS1_PADDING);
+    // int maxLength = RSA_public_encrypt(0, nullptr, nullptr, rsa, RSA_PKCS1_PADDING);
 
-    std::cout << "Tamaño máximo del bloque de encriptación: " << maxLength << " bytes" << std::endl;
+    // std::cout << "Tamaño máximo del bloque de encriptación: " << maxLength << " bytes" << std::endl;
 
     std::string encryptedMessage;
     encryptedMessage.resize(RSA_size(rsa));
