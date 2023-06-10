b3542b107dff833c3cfa9d50cb0d8dcf66022ae6
manuCR <manuelarroyoportilla@gmail.com>
2023-06-10 14:13:38 -0600

    prueba

--- Client.cpp
+++ Client.cpp
@@ -3,14 +3,15 @@
 Client::Client(std::string address, int port, Feedback * feedback, std::string bindIp) {
   socket = new Socket(address, port, feedback);
   working = socket->bindTo(bindIp);
-  working = socket->connectTo();
 }
 
 void Client::send(std::vector<unsigned char> message) {
+  socket->connectTo();
   return socket->send(message);
 }
 
 void Client::send(std::string message) {
+  socket->connectTo();
   return socket->send(message);
 }
 
